99e3f761af192123af48e473343d7068232437fa
BANE590 <69880608+BANE590@users.noreply.github.com>
2020-08-21 08:48:53 +0700

    Add files via upload

---
+++ mobile.php
@@ -0,0 +1,26 @@
+<?php
+
+Username = "Mobile";
+Password = "Diamonds";
+
+system('clear');
+echo "Login dulu\n";e Legend";
+$password = "Diamonds";
+
+system('clear');
+echo "Login dulu\n";
+echo "username : ";
+$user = trim(fgets(STDIN));
+echo "password : ";
+$user = trim(fgets(STDIN));
+if($user == $username && $pass == $password)
+
+{
+echo "Login Sukse\n";
+}
+else
+{
+echo "Login Gagal\n";
+}
+
+?>
