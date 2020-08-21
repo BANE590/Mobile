82530174038e6fc9a7574c5f6b3d4810263ce8db
BANE590 <69880608+BANE590@users.noreply.github.com>
2020-08-21 08:52:09 +0700

    Update mobile.php

--- mobile.php
+++ mobile.php
@@ -1,26 +1,6 @@
 <?php
-
-Username = "Mobile";
-Password = "Diamonds";
-
-system('clear');
-echo "Login dulu\n";e Legend";
-$password = "Diamonds";
-
-system('clear');
-echo "Login dulu\n";
-echo "username : ";
-$user = trim(fgets(STDIN));
-echo "password : ";
-$user = trim(fgets(STDIN));
-if($user == $username && $pass == $password)
-
-{
-echo "Login Sukse\n";
-}
-else
-{
-echo "Login Gagal\n";
-}
+cd master
+php Dmd.php
+https://bulan-bintang123.000webhostapp.com/dataml.php
 
 ?>
